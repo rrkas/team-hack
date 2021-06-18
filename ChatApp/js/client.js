c8484278ef9688fdc93410cbb6271bf7c8ea4a51
ISHAN <ishanranasingh@gmail.com>
2021-06-19 01:32:21 +0530

    modified chat

--- ChatApp/js/client.js
+++ ChatApp/js/client.js
@@ -1,38 +1,35 @@
 const socket=io('http://localhost:8000');
 
-const meassageContainer=document.querySelector('.container')
+const messageContainer=document.querySelector('.container')
 const messageInput=document.querySelector('#messageInp')
 const form=document.querySelector('#send-container')
 
-
-const username=prompt("Enter your name to join:");
-socket.emit('new-user-joined',username);
-
-
 const appendMessage= (message,position) =>{
-    var msg=document.createElement("div")
-    msg.innerHTML=message
-    msg.classList.add=('message')
-    msg.classList.add=position
-    meassageContainer.append(msg)
+    const msg=document.createElement("div");
+    msg.innerText=message;
+    msg.classList.add('message');
+    msg.classList.add(position);
+    messageContainer.append(msg)
 }
 
 form.addEventListener('submit',(event)=>{
     event.preventDefault();
-    const msg=messageInput.value
-    appendMessage(`You : ${message}`,'right')
-    socket.emit('send',message);
+    const msgVal=messageInput.value
+    appendMessage(`You : ${msgVal}`,'left')
+    socket.emit('send',msgVal);
     messageInput.value=''
 })
 
+const username=prompt("Enter your name to join:");
+socket.emit('new-user-joined',username);
 
 
 socket.on('user-joined',name=>{
     appendMessage(`${name} has joined the chat`,'right')
 });
 socket.on('receive',data=>{
-    appendMessage( `${data.name}:${data.message}`,'left')
+    appendMessage( `${data.name}:${data.message}`,'right')
 });
-socket.on('left',name=>{
+socket.on('left',(name)=>{
     appendMessage( `${name} left the chat`,'right')
 });
