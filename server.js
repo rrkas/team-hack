1db8687a8701632105caddaab7cf36862caf5891
Rohnak Agarwal <rrka79wal@gmail.com>
2021-06-18 22:49:49 +0530

    chat & video call merge

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const app = express()
 const server = require('http').Server(app)
 const io = require('socket.io')(server)
 const { v4: uuidV4 } = require('uuid')
+const users={};
 
 app.set('view engine', 'ejs')
 app.use(express.static('public'))
@@ -18,12 +19,22 @@ app.get('/:room', (req, res) => {
 io.on('connection', socket => {
   socket.on('join-room', (roomId, userId) => {
     socket.join(roomId)
-    socket.to(roomId).broadcast?.emit('user-connected', userId)
-
-    socket.on('disconnect', () => {
-      socket.to(roomId).broadcast?.emit('user-disconnected', userId)
-    })
+    socket.broadcast.emit('user-connected', userId)
+    socket.on('disconnect',message=>{
+      socket.broadcast.emit('user-disconnected', userId)
+    });
   })
+  socket.on('new-user-joined',(name)=>{
+      users[socket.id]=name
+      socket.broadcast.emit('user-joined',name)
+  });
+  socket.on('send',message=>{
+      socket.broadcast.emit('receive',{message:message,name:users[socket.id]})
+  });
+  socket.on('disconnect',message=>{
+      socket.broadcast.emit('left',users[socket.id]);
+      delete users[socket.id]
+  });
 })
 
 server.listen(3000)
